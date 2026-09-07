6d4d777ff76fae816691e2a1cd77e713d2e4dbc0
Jureepron <chureepornchomloke@gmail.com>
2026-09-07 12:10:19 +0700

    Configure GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    baseURL: '/Next4Aon/' // เปลี่ยนชื่อ Next4Aon เป็นชื่อ Repository ของคุณถ้าตั้งชื่ออื่น
+  }
 })
